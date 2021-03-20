53fb9dbf9bc309a144860e027bb6343a29da0965
Harrison Ferrone <hferrone@gmail.com>
2021-03-19 20:10:46 -0600

    Add Ch_09 starter content

---
+++ Ch_10_Starter/Assets/Scripts/Weapon.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Time for action - creating a weapon struct
+public struct Weapon
+{
+    public string name;
+    public int damage;
+
+    public Weapon(string name, int damage)
+    {
+        this.name = name;
+        this.damage = damage;
+    }
+
+    public void PrintWeaponStats()
+    {
+        Debug.LogFormat("Weapon: {0} - {1} DMB", name, damage);
+    }
+}
