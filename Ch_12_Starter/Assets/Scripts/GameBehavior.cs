efa490c5e010536646c3c9c2197dda457653c298
Harrison Ferrone <hferrone@gmail.com>
2021-03-21 12:54:23 -0600

    Add Ch_12 starter content

---
+++ Ch_12_Starter/Assets/Scripts/GameBehavior.cs
@@ -0,0 +1,136 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Time for action - pausing and restarting
+using UnityEngine.SceneManagement;
+
+// Time for action - using an extension method
+using CustomExtensions;
+
+public class GameBehavior : MonoBehaviour
+{
+    // Time for action storing collected items
+    public Stack<string> lootStack = new Stack<string>();
+
+    // Time for action - adopting an interface
+    private string _state;
+    public string State
+    {
+        get { return _state; }
+        set { _state = value; }
+    }
+
+    // Time for action - creating a game manager
+    private int _itemsCollected = 0;
+    private int _playerHP = 10;
+
+    // Time for action - adding UI elements
+    public string labelText = "Collect all 4 items and win your freedom!";
+    public int maxItems = 4;
+
+    // Time for action - winning the game
+    public bool showWinScreen = false;
+
+    // Time for action - updating the game manager
+    public bool showLossScreen = false;
+
+    // Time for action - adding backing variables
+    public int Items
+    {
+        get { return _itemsCollected; }
+        set
+        {
+            _itemsCollected = value;
+
+            if (_itemsCollected >= maxItems)
+            {
+                labelText = "You've found all the items!";
+                showWinScreen = true;
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                labelText = "Item found, only " + (maxItems - _itemsCollected) + " more to go!";
+            }
+        }
+    }
+
+    public int HP
+    {
+        get { return _playerHP; }
+        set
+        {
+            _playerHP = value;
+
+            if (_playerHP <= 0)
+            {
+                labelText = "You want another life with that?";
+                showLossScreen = true;
+                Time.timeScale = 0;
+            }
+            else
+            {
+                labelText = "Ouch... that's got hurt.";
+            }
+
+        }
+    }
+
+    void Start()
+    {
+        Initialize();
+    }
+
+    public void Initialize()
+    {
+        _state = "Manager initialized..";
+        _state.FancyDebug();
+        Debug.Log(_state);
+
+        lootStack.Push("Sword of Doom");
+        lootStack.Push("HP+");
+        lootStack.Push("Golden Key");
+        lootStack.Push("Winged Boot");
+        lootStack.Push("Mythril Bracers");
+    }
+
+    public void PrintLootReport()
+    {
+        // Time for action - the last item collected
+        var currentItem = lootStack.Pop();
+        var nextItem = lootStack.Peek();
+
+        Debug.LogFormat("You got a {0}! You've got a good chance of finding a {1} next!", currentItem, nextItem); 
+        Debug.LogFormat("There are {0} random loot items waiting for you!", lootStack.Count); 
+    }   
+
+    void RestartLevel()
+    {
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1.0f;
+    }
+
+    void OnGUI()
+    {
+        if (showWinScreen)
+        {
+            if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 100), "YOU WON!"))
+            {
+                Utilities.RestartLevel(0);
+            }
+        }
+
+        if (showLossScreen)
+        {
+            if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 100), "You lose..."))
+            {
+                Utilities.RestartLevel();
+            }
+        }
+
+        GUI.Box(new Rect(20, 20, 150, 25), "Player Health:" + _playerHP);
+        GUI.Box(new Rect(20, 50, 150, 25), "Items Collected: " + _itemsCollected);
+        GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height - 50, 300, 50), labelText);
+    }
+}
