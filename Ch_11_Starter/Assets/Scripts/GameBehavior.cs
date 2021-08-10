58a25b6747ffef7aa572efa375c6683feed07fae
Harrison Ferrone <hferrone@gmail.com>
2021-08-10 15:31:56 -0600

    Update Ch11 starter project

--- Ch_11_Starter/Assets/Scripts/GameBehavior.cs
+++ Ch_11_Starter/Assets/Scripts/GameBehavior.cs
@@ -1,117 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-// Time for action - pausing and restarting
 using UnityEngine.SceneManagement;
-
-// Time for action - using an extension method
-using CustomExtensions;
+using UnityEngine.UI;
 
 public class GameBehavior : MonoBehaviour
 {
-    // Time for action - adopting an interface
-    private string _state;
-    public string State
-    {
-        get { return _state; }
-        set { _state = value; }
-    }
+    public int maxItems;
+    public Text HealthText;
+    public Text ItemText;
+    public Text ProgressText;
+    public Button WinButton;
+    public Button LossButton;
 
-    // Time for action - creating a game manager
     private int _itemsCollected = 0;
-    private int _playerHP = 10;
-
-    // Time for action - adding UI elements
-    public string labelText = "Collect all 4 items and win your freedom!";
-    public int maxItems = 4;
-
-    // Time for action - winning the game
-    public bool showWinScreen = false;
-
-    // Time for action - updating the game manager
-    public bool showLossScreen = false;
-
-    // Time for action - adding backing variables
     public int Items
     {
         get { return _itemsCollected; }
         set
         {
             _itemsCollected = value;
+            ItemText.text = "Items Collected: " + Items;
 
             if (_itemsCollected >= maxItems)
             {
-                labelText = "You've found all the items!";
-                showWinScreen = true;
-                Time.timeScale = 0f;
+                WinButton.gameObject.SetActive(true);
+                UpdateScene("You've found all the items!");
             }
             else
             {
-                labelText = "Item found, only " + (maxItems - _itemsCollected) + " more to go!";
+                ProgressText.text = "Item found, only " + (maxItems - _itemsCollected) + " more to go!";
             }
         }
     }
 
+    private int _playerHP = 10;
     public int HP
     {
         get { return _playerHP; }
         set
         {
             _playerHP = value;
+            HealthText.text = "Player Health: " + HP;
 
             if (_playerHP <= 0)
             {
-                labelText = "You want another life with that?";
-                showLossScreen = true;
-                Time.timeScale = 0;
+                LossButton.gameObject.SetActive(true);
+                UpdateScene("You want another life with that?");
             }
             else
             {
-                labelText = "Ouch... that's got hurt.";
+                ProgressText.text = "Ouch... that's got hurt.";
             }
 
+            Debug.LogFormat("Lives: {0}", _playerHP);
         }
     }
 
-    void Start()
+    public void UpdateScene(string updatedText)
     {
-        Initialize();
+        ProgressText.text = updatedText;
+        Time.timeScale = 0f;
     }
 
-    public void Initialize()
-    {
-        _state = "Manager initialized..";
-        _state.FancyDebug();
-        Debug.Log(_state);
-    }
-
-    void RestartLevel()
+    public void RestartScene()
     {
         SceneManager.LoadScene(0);
-        Time.timeScale = 1.0f;
-    }
-
-    void OnGUI()
-    {
-        if (showWinScreen)
-        {
-            if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 100), "YOU WON!"))
-            {
-                Utilities.RestartLevel(0);
-            }
-        }
-
-        if (showLossScreen)
-        {
-            if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 100), "You lose..."))
-            {
-                Utilities.RestartLevel();
-            }
-        }
-
-        GUI.Box(new Rect(20, 20, 150, 25), "Player Health:" + _playerHP);
-        GUI.Box(new Rect(20, 50, 150, 25), "Items Collected: " + _itemsCollected);
-        GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height - 50, 300, 50), labelText);
+        Time.timeScale = 1f;
     }
 }
