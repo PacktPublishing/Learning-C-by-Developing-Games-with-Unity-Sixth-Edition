caebe9745b3ec46c50ce7e9c4bf7bb5af7d673bb
Harrison Ferrone <hferrone@gmail.com>
2021-10-01 14:17:13 -0600

    Fix compile error in Ch_11_Starter and update Player to be kinematic

--- Ch_11_Starter/Assets/Scripts/GameBehavior.cs
+++ Ch_11_Starter/Assets/Scripts/GameBehavior.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using CustomExtensions;
 
 public class GameBehavior : MonoBehaviour, IManager
 {
